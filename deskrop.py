011a5c3686da01dcb16f0c26815a702554aa3b6f
MattisJarans <matiss.jarans@gmail.com>
2024-10-04 11:36:51 +0300

    Add files via upload

---
+++ deskrop.py
@@ -0,0 +1,19 @@
+a = 5
+b = 10
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a ** b)
+print(a // b)
+print(a % b)
+
+a = input('Enter number a:')
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a ** b)
+print(a // b)
+print(a % b)
